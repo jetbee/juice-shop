4437733ed9cb0fbcf903fe1a7b53ca617631016a
jetbee <jetbee0z@gmail.com>
2025-02-25 12:04:22 +0900

    confirm ssrf

--- routes/fileServer.ts
+++ routes/fileServer.ts
@@ -12,19 +12,24 @@ import * as utils from '../lib/utils'
 const security = require('../lib/insecurity')
 
 module.exports = function servePublicFiles () {
-  return ({ params, query }: Request, res: Response, next: NextFunction) => {
+  return ({ params, query, headers }: Request, res: Response, next: NextFunction) => {
     const file = params.file
-
+    const host = headers.host
+    var hostname = ""
+    if (host) {
+      hostname = host.split(':')[0]; // ポート番号を削除
+    }
     if (!file.includes('/')) {
-      verify(file, res, next)
+      verify(file, hostname, res, next)
     } else {
       res.status(403)
       next(new Error('File names cannot contain forward slashes!'))
     }
   }
 
-  function verify (file: string, res: Response, next: NextFunction) {
-    if (file && (endsWithAllowlistedFileType(file) || (file === 'incident-support.kdbx'))) {
+  function verify (file: string, hostname: string, res: Response, next: NextFunction) {
+
+    if (file && (hostname === 'localhost' || hostname === '127.0.0.1') || (endsWithAllowlistedFileType(file) || (file === 'incident-support.kdbx'))) {
       file = security.cutOffPoisonNullByte(file)
 
       challengeUtils.solveIf(challenges.directoryListingChallenge, () => { return file.toLowerCase() === 'acquisitions.md' })
